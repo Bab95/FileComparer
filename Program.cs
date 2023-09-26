90ee499d5558e444a0139cea53035aa8abc07020
Babnish Vyas <babnishvyas@microsoft.com>
2023-09-26 12:32:28 +0530

    cleaning up mess

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        //string file1 = @"C:\Users\Babnish\Downloads\large\bible-test1";
-        //string file2 = @"C:\Users\Babnish\Downloads\large\bible-test2";
-        //new SequentialFileComparer(file1, file2).Compare();
         new FileComparerMain().AppMain(args).Wait();
     }
 }
